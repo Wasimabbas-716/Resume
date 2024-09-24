053593a85bcb8ff5c334b5f82bb40c1e4d630b39
Wasimabbas-716 <wasimabbas0051@gmail.com>
2024-09-24 18:27:32 +0500

    Add files via upload

---
+++ Resume Maker/script.js
@@ -0,0 +1,17 @@
+// handle the pictur
+
+  const imageInput = document.getElementById('image-input');
+  const profileImg = document.getElementById('profile-img');
+
+  imageInput.addEventListener('change', function(e) {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = function(e) {
+      profileImg.src = e.target.result;
+    };
+
+    reader.readAsDataURL(file);
+  });
+
+  
